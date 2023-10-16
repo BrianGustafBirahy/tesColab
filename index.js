b84251662c5362046066bdd4009290c2725f0884
BrianGustafBirahy <s22110506@student.unklab.ac.id>
2023-10-16 21:59:57 +0800

    tes

---
+++ index.js
@@ -0,0 +1,12 @@
+// Basic Git & Github
+
+console.log("Kode program hari senin");
+
+console.log("Krismarvel Ticoalu");
+
+console.log("Gustaf");
+
+console.log("Stenly Adam");
+console.log("Gustaf");
+
+console.log("Krismarvel Ticoalu");
